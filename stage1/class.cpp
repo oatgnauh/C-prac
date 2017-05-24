b5d6b591f0837b25b450a5ef0817ac942b45faee
pythonhunter <892145616@qq.com>
2017-05-24 15:38:57 +0800

    Update class.cpp

--- stage1/class.cpp
+++ stage1/class.cpp
@@ -1,1 +1,15 @@
 #include<iostream>
+using namespace std;
+class Car{
+public :
+		void move():
+		void stop(){
+			cout<<"car stoped"<<endl;
+		}
+
+		void setupspeed(short s);
+		void setweight(short w);
+private:
+		short speed;
+		short weight;
+};
