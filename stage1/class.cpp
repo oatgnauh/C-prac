097ffcbe05ebc5eec3f16e6fa5add9a329287ae4
pythonhunter <892145616@qq.com>
2017-05-24 15:50:53 +0800

    Update class.cpp

--- stage1/class.cpp
+++ stage1/class.cpp
@@ -1,15 +1,19 @@
 #include<iostream>
 using namespace std;
-class Car{
+//定义一个类
+class Car{		
 public :
-		void move():
-		void stop(){
+		void move():		//声明成员函数
+		void stop(){		//成员函数可以在声明部分实现，也可以在实现部分实现
 			cout<<"car stoped"<<endl;
 		}
 
 		void setupspeed(short s);
 		void setweight(short w);
 private:
-		short speed;
+		short speed;		//声明成员数据
 		short weight;
 };
+
+//类的实现部分
+
