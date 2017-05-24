da7aa2db6f4525f66bfa368ebbde005e5d61bbe8
pythonhunter <892145616@qq.com>
2017-05-24 16:09:08 +0800

    Update class.cpp

--- stage1/class.cpp
+++ stage1/class.cpp
@@ -8,7 +8,7 @@ public :
 			cout<<"car stoped"<<endl;
 		}
 
-		void setupspeed(short s);
+		void setspeed(short s);
 		void setweight(short w);
 private:
 		short speed;		//声明成员数据
@@ -17,3 +17,22 @@ private:
 
 //类的实现部分
 
+void Car::setspeed(short s){		//双冒号此处的用法：表明setspeed这个函数是Car的成员函数
+        speed =s;
+	cout<<"speed is fast"<<endl;
+}
+
+void Car::setweight(short w){
+        weight =w;
+	cout<<"I don't know the weight,,,hahaha"<<endl;
+}
+void Car::move(){
+	cout<<"roll out!"<<endl;
+}
+
+int main(){
+        
+
+
+
+}
